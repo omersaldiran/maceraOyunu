77519f2bcf795fea454646f09051dddb99adc832
omersaldiran <osaldiran@gmail.com>
2022-05-17 10:27:54 +0300

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Game {
+    private Scanner input = new Scanner(System.in);
+    public void start(){
+        System.out.println("Macera oyununa hoşgeldiniz!");
+        System.out.print("Lütfen bir isim giriniz:");
+        String playerName = input.nextLine();
+        Player player = new Player(playerName);
+        System.out.println(player.getName() + " adaya hoşgeldin!");
+        System.out.println("Bu adada yaşananların hepsi gerçek!");
+        System.out.println("Lütfen bir karakter seçiniz!");
+        player.selectChar();
+
+    }
+}
---
+++ src/Samurai.java
@@ -0,0 +1,5 @@
+public class Samurai extends GameChar{
+    public Samurai(int damage, int health, int money) {
+        super(5, 21, 15);
+    }
+}
